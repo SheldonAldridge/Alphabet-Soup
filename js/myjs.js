4f665033a399b4622cabb8fb0188e792362e979b
Sheldon <sheldonaldridge@yahoo.com>
2022-03-10 22:06:24 +0200

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -1,9 +1,21 @@
-let userInput = prompt("Insert word/name here!");
-console.log(userInput);
+let str;
 
+        str = prompt("Insert word/name here!");
 
-function alphabetSoup(userInput)
-{
-    userInput.sort(userInput);
-    console.log(userInput);
-}
+        console.log(str);
+
+        function alphabetSoup(str) {
+
+            // convert string to array
+            str = str.split("");
+
+            // once its an array you can sort it
+            str.sort();
+            
+            // now that it is sorted we can use join to convert it back into a String
+            str = str.join("")
+
+            return str;        
+        };
+
+        console.log(alphabetSoup(str));
